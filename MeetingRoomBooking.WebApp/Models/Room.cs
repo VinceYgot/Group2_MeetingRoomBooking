8e48e54463e769b96aebd6f7a99dfb84220f2ae4
Mistypeaks13 <kjamesgabriel@gmail.com>
2024-10-07 19:18:36 +0800

    Creating Models for Rooms Users and Bookings. Made a working crud for Users but cannot handle nulls yet

---
+++ MeetingRoomBooking.WebApp/Models/Room.cs
@@ -0,0 +1,14 @@
+﻿namespace MeetingRoomBooking.WebApp.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string RoomName { get; set; }
+        public string RoomDescription { get; set; }
+        public string[] Ammenities { get; set; }
+
+
+
+        
+    }
+}
